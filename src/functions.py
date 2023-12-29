a8d1754343e3d255d4db7bb42d707ea12d0c56e8
Julia Pohl <jpohl_cat@ada.cs.pdx.edu>
2023-12-28 18:18:27 -0800

    new function (multiply)

--- src/functions.py
+++ src/functions.py
@@ -4,3 +4,7 @@ def add_numbers(a, b):
 
 def subtract_numbers(a, b):
     return a - b
+
+
+def multiply_numbers(a, b):
+    return a * b
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,7 @@
 from src.functions import (
     add_numbers,
     subtract_numbers,
+    multiplly_numbers,
 )
 
 
@@ -9,4 +10,8 @@ def test_add_numbers():
 
 
 def test_subtract_numbers():
-    assert subtract_numbers(3, 2) == 1
+    assert subtract_numbers(4, 2) == 1
+
+
+def test_multiply_numbers():
+    assert multiply_numbers(2, 3) == 6
